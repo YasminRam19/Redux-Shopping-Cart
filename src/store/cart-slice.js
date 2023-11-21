9fa009359a040dfb72ead798bc878c5d467fe879
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-11-20 21:04:51 -0600

    Add cart slice and ADD item to cart reducer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,12 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const redux = require("redux");
-const initialState = { items: [] };
+const initialState = { items: [], totalQuantity: 0 };
 
 createSlice({
   name: "cart",
   initialState: initialState,
   reducers: {
-    add(state) {},
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          title: newItem.title,
+        });
+      } else {
+        existingItem.quanity++;
+        existingItem.totalPrice = totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state) {},
   },
 });
