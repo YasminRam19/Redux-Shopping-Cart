17be249a31d821fce4eb3befa944438e88215f05
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-11-22 23:11:36 -0600

    Dispatching actions to display cart items, and enable add and remove items on the shopping cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -12,7 +12,7 @@ const cartSlice = createSlice({
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -27,10 +27,10 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
-      if (existingItem.quanity === 1) {
+      if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
-        existingItem.quanity--;
+        existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
